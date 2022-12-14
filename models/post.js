d621a8d0e332d7ec8fe4a6957b6e293677c0d64b
Aaron Aco <aaco@village88.com>
2022-12-14 17:44:16 +0800

    add comment

--- models/post.js
+++ models/post.js
@@ -1,9 +1,20 @@
 const DBConnection = require("./connection.js");
 const Mysql = require("mysql");
 const { checkFields } = require("../helpers/index.helper");
+/**
+ * @class Post
+ */
 class Post{
 
-    
+    /**
+     * DOCU: Function to retrieve both message and comments from the database using nested queries. <br/>
+     * Triggered: When loading the wall page after the successfull logged in of the user. <br/>
+     * Last Updated Date: December 14, 2022
+     * @async
+     * @function
+     * @returns {object} - response_data {status: true|false, result: {object} | [array], error: any }
+     * @author Aaron Aco
+     */
      async retrieve () {
         let response_data = { status: false, result: null, error: null };
         let query = Mysql.format(`
@@ -30,6 +41,16 @@ class Post{
         return response_data; 
     }
 
+    /**
+     * DOCU: Function to create a new message in the database. <br/>
+     * Triggered: When the create message proccess is triggered. <br/>
+     * Last Updated Date: December 14, 2022.
+     * @async
+     * @function 
+     * @param {integer} user_id  - Requires the id of the user from the session
+     * @param {object} form_data - Requires the message
+     * @returns {object} response_data - {status: true|false, result: {object} | [array], error: any}
+     */
      async create (user_id,form_data) {
         let response_data = { status: false, result: null, error: null };
         let check_fields = checkFields(
@@ -52,6 +73,13 @@ class Post{
         return response_data; 
     }
 
+    /**
+     * DOCU: Function to create a new comment to a message in the database. <br/>
+     * Triggered: When the create comment is triggered. <br/>
+     * @param {integer} user_id  - Requires the user id from the session
+     * @param {object} form_data  - Requires the comment
+     * @returns {object} - response_data {status: false|true, result: {object} | [array], error: any}
+     */
      async reply (user_id,form_data) {
         let response_data = { status: false, result: null, error: null };
         let check_fields = checkFields(
@@ -75,6 +103,16 @@ class Post{
         return response_data; 
     }
 
+    /**
+     * DOCU: Function to update the comment isDeleted column value to true/1 in the database. <br/>
+     * Triggered: When delete comment proccess is triggered. <br/>
+     * Last Updated Date: December 14, 2022.
+     * @async
+     * @function
+     * @param {object} {comment_id} - Requires the comment id 
+     * @returns {object} response_data {status: true|false, result: {object} | [array], error: any}
+     * @author Aaron Aco
+     */
      async deleteComment ({comment_id}) {
         let response_data = { status: false, result: null, error: null };
         let query = Mysql.format(`
@@ -88,6 +126,16 @@ class Post{
         return response_data; 
     }
 
+    /**
+     * DOCU: Function to update the message isDeleted column value to true/1 in the database. <br/>
+     * Triggered: When delete message proccess is triggered. <br/>
+     * Last Updated Date: December 14, 2022.
+     * @async
+     * @function
+     * @param {object} {message_id} - Requires the message id 
+     * @returns {object} response_data {status: true|false, result: {object} | [array], error: any}
+     * @author Aaron Aco
+     */
     async deleteMessage ({message_id}) {
         let response_data = { status: false, result: null, error: null };
         let query = Mysql.format(`
@@ -100,7 +148,6 @@ class Post{
 
         return response_data; 
     }
-
-    
 }
+
 module.exports = Post;
